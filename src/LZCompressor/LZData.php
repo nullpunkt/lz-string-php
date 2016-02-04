6ec2819d82cf4f882b2993df0d6a21da87a29218
juhasz.adam <juhasz.adam@virgo.hu>
2016-02-04 13:07:17 +0100

    Overhaul

---
+++ src/LZCompressor/LZData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LZCompressor;
+
+class LZData
+{
+    /**
+     * @var
+     */
+    public $str;
+
+    /**
+     * @var
+     */
+    public $val;
+
+    /**
+     * @var int
+     */
+    public $position = 0;
+
+    /**
+     * @var int
+     */
+    public $index = 1;
+}
